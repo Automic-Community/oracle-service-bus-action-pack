ad54f75d50c899ea042701490280583f0f0d3f28
unknown <shrutinambiar@SHRUTI3794.Nagarro.local>
2016-04-21 11:19:56 +0530

    PMARA-2127<PMARA-2213>:modified create_session python script, process tab for the same action

--- tools/osbtool/resources/pythonscripts/pck_oracle_create_session.py
+++ tools/osbtool/resources/pythonscripts/pck_oracle_create_session.py
@@ -4,16 +4,23 @@ from com.bea.wli.sb.management.configuration import ALSBConfigurationMBean
 from com.bea.wli.config import Ref
 
 try:
-		connect(sys.argv[1], sys.argv[2], 't3://'+ sys.argv[3] + ':' + sys.argv[4])
+		username = sys.argv[1]
+		password = sys.argv[4]
+		hostname = sys.argv[2]
+		sessionName = sys.argv[3]
+		
+		if len(sys.argv) == 6:
+				connect(username, password, 't3://'+ hostname + ':' + sys.argv[5])
+		else:
+				connect(username, password, 't3://'+ hostname)
+		
 		domainRuntime()
 
 		# obtain session management mbean to create a session.
-		# This mbean instance can be used more than once to
-		# create/discard/commit many sessions
 		sessionMBean = findService(SessionManagementMBean.NAME,SessionManagementMBean.TYPE)
 		
 		# create a session
-		sessionMBean.createSession(sys.argv[5])
+		sessionMBean.createSession(sessionName)
 except:
 		print "Unexpected error: ", sys.exc_info()[0]
 		dumpStack()
