e443435989cd7781226ae05f25d6c5b60e461347
unknown <vivekmalhotra@vivek2702.Nagarro.local>
2016-04-21 13:44:15 +0530

    PMARA-2128<PMARA-2219>: added action ACTIVATE_SESSION and DEACTIVATE_SESSION

---
+++ tools/osbtool/resources/pythonscripts/pck_oracle_discard_session.py
@@ -0,0 +1,20 @@
+import wlstModule
+from com.bea.wli.sb.management.configuration import SessionManagementMBean
+from com.bea.wli.sb.management.configuration import ALSBConfigurationMBean
+from com.bea.wli.config import Ref
+
+try:
+		connect(sys.argv[1], sys.argv[2], 't3://'+ sys.argv[3] + ':' + sys.argv[4])
+		domainRuntime()
+
+		# obtain session management mbean to create a session.
+		# This mbean instance can be used more than once to
+		# create/discard/commit many sessions
+		sessionMBean = findService(SessionManagementMBean.NAME,SessionManagementMBean.TYPE)
+		
+		# activate a session and provide a description to it.
+		sessionMBean.discardSession(sys.argv[5])
+except:
+		print "Unexpected error: ", sys.exc_info()[0]
+		dumpStack()
+		raise
