e480ab47a9e092abc83fc3bb8aec43097d052122
unknown <shrutinambiar@SHRUTI3794.Nagarro.local>
2016-04-26 17:59:00 +0530

    PMARA-2130<PMARA-2235>: added disconnect command in all the python scripts, modified the shared include escape_arg_value, added t3s, iiop, iiops protocols in shared vara

--- tools/osbtool/resources/pythonscripts/pck_osb_import_resource.py
+++ tools/osbtool/resources/pythonscripts/pck_osb_import_resource.py
@@ -8,69 +8,69 @@ from java.io import FileInputStream
 
 import sys
 
+connectObj = False
 try:
+	try:
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    url = sys.argv[3]
-    sessionName = sys.argv[4]
-    jarfilepath = sys.argv[5]
-    print "================================================================================"
-    connect(username, password, url)
-    domainRuntime()
+		username = sys.argv[1]
+		password = sys.argv[2]
+		url = sys.argv[3]
+		sessionName = sys.argv[4]
+		jarfilepath = sys.argv[5]
+		print "================================================================================"
+		connectObj = connect(username, password, url)
+		connectObj = True
+		domainRuntime()
 
-    # obtain session management mbean to create a session.
-    # This mbean instance can be used more than once to
-    # create/discard/commit many sessions
+		# obtain session management mbean to create a session.
+		# This mbean instance can be used more than once to
+		# create/discard/commit many sessions
 
-    sessionMBean = findService(SessionManagementMBean.NAME,
-                               SessionManagementMBean.TYPE)
+		sessionMBean = findService(SessionManagementMBean.NAME, SessionManagementMBean.TYPE)
 
-    # obtain the ALSBConfigurationMBean instance that operates
-    # on the session that has just been created. Notice that
-    # the name of the mbean contains the session name.
+		# obtain the ALSBConfigurationMBean instance that operates
+		# on the session that has just been created. Notice that
+		# the name of the mbean contains the session name.
 
-    alsbSession = findService(ALSBConfigurationMBean.NAME + '.'
-                              + sessionName,
-                              ALSBConfigurationMBean.TYPE)
+		alsbSession = findService(ALSBConfigurationMBean.NAME + '.'+ sessionName, ALSBConfigurationMBean.TYPE)
 
-    # read a resource config file (example a jar) into bytes and uploading it
+		# read a resource config file (example a jar) into bytes and uploading it
 
-    if alsbSession is None:
-        print 'Session %s not found ' % sessionName
-        raise 
-    print 'Reading imported jar file....'
+		if alsbSession is None:
+			print 'Session %s not found ' % sessionName
+			raise 
+		print 'Reading imported jar file....'
 
-    fh = open(jarfilepath, 'rb')
-    filebytes = fh.read()
-    fh.close()
+		fh = open(jarfilepath, 'rb')
+		filebytes = fh.read()
+		fh.close()
 
-    # Uploading jar file
+		# Uploading jar file
 
-    print 'Uploading Jar file.....'
-    alsbSession.uploadJarFile(filebytes)
-    print 'Jar Uploaded.......'
+		print 'Uploading Jar file.....'
+		alsbSession.uploadJarFile(filebytes)
+		print 'Jar Uploaded.......'
 
-    print 'ALSB Project will now get imported'
-    jarInfo = alsbSession.getImportJarInfo()
-    importPlan = jarInfo.getDefaultImportPlan()
-    result = alsbSession.importUploaded(importPlan)
+		print 'ALSB Project will now get imported'
+		jarInfo = alsbSession.getImportJarInfo()
+		importPlan = jarInfo.getDefaultImportPlan()
+		result = alsbSession.importUploaded(importPlan)
 
-    # Print out status and build a list of created references.
+		# Print out status and build a list of created references.
 
-    if result.getFailed().isEmpty() == false:
-        print 'One or more resources could not be imported properly'
-        raise
-    else:
-        print 'The following resources have been imported: '
-        for successEntry in result.getImported():
-            print '-- %s' % successEntry.toString()
-except:
+		if result.getFailed().isEmpty() == false:
+			print 'One or more resources could not be imported properly'
+			raise
+		else:
+			print 'The following resources have been imported: '
+			for successEntry in result.getImported():
+				print '-- %s' % successEntry.toString()
+	except:
 
-    print 'Unexpected error while importing resource : ', \
-        sys.exc_info()[0]
-    dumpStack()
-    raise
-
-
-			
+		print 'Unexpected error while importing resource : ', \
+			sys.exc_info()[0]
+		dumpStack()
+		raise
+finally:
+		if connectObj is True:
+			disconnect()			
