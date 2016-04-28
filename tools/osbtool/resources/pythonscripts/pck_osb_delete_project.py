50bc8d5251fe64424d08b0919079e191dd0cf3a6
unknown <vivekmalhotra@vivek2702.Nagarro.local>
2016-04-28 09:55:34 +0530

    PMARA-2153<PMARA-2326>: added AE documentation. updated python for attribute validation.

--- tools/osbtool/resources/pythonscripts/pck_osb_delete_project.py
+++ tools/osbtool/resources/pythonscripts/pck_osb_delete_project.py
@@ -2,40 +2,45 @@ import wlstModule
 from com.bea.wli.sb.management.configuration import SessionManagementMBean
 from com.bea.wli.sb.management.configuration import ALSBConfigurationMBean
 from com.bea.wli.config import Ref
+from java.util import Collections
+from com.bea.wli.sb.util import Refs
 
 def deleteProject(alsbConfigurationMBean, projectName):
      try:
-          print "Trying to remove " + projectName
           projectRef = Ref(Ref.PROJECT_REF, Ref.DOMAIN, projectName)                  
           if alsbConfigurationMBean.exists(projectRef):
-               print "#### removing OSB project: " + projectName
+               print 'INFO: Removing OSB project: ' + projectName
                alsbConfigurationMBean.delete(Collections.singleton(projectRef))
-               print "#### removed project: " + projectName
+               print 'INFO: Removed OSB project: ' + projectName
           else:
-               failed = "OSB project <" + projectName + "> does not exist"
-               print failed
-          print
+               raise AttributeError('No OSB project exists with name ' + projectName)
      except:
-          print "Error while removing project:", sys.exc_info()[0]
           raise
+
+connectObj = False
 try:
-	username = sys.argv[1]
-	password = sys.argv[2]
-	url = sys.argv[3]
-	sessionName = sys.argv[4]
-	projectName = sys.argv[5]
+	try:
+		username = sys.argv[1]
+		password = sys.argv[2]
+		url = sys.argv[3]
+		sessionName = sys.argv[4]
+		projectName = sys.argv[5]
 		
-	# connect to OSB with URL endpoint, username and password
-	connect(username, password, url)
-	domainRuntime()
+		# connect to OSB with URL endpoint, username and password
+		connectObj = connect(username, password, url)
+		connectObj = True
+		domainRuntime()
 
-	# obtain session management mbean to activate a session.
-	sessionMBean = findService(SessionManagementMBean.NAME, SessionManagementMBean.TYPE)
-	
-	# obtain ALSBConfigurationMBean to delete project
-	alsbConfigurationMBean = findService(ALSBConfigurationMBean.NAME + "." + sessionName, ALSBConfigurationMBean.TYPE)
-	deleteProject(alsbConfigurationMBean, projectName)
-
-
-except:
-	raise "ERROR: Please check Input parameters.", sys.exc_info()[0]
+		# obtain ALSBConfigurationMBean to delete project
+		alsbConfigurationMBean = findService(ALSBConfigurationMBean.NAME + "." + sessionName, ALSBConfigurationMBean.TYPE)
+		if alsbConfigurationMBean is not None:
+			deleteProject(alsbConfigurationMBean, projectName)
+		else:
+			raise AttributeError('No session exists with name ' + sessionName) 
+	except:
+		print "ERROR : Unable to delete Project. Please check input parameters ", sys.exc_info()[0]
+		dumpStack()
+		raise
+finally:
+		if connectObj is True:
+			disconnect()	
