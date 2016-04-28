a64d4ba483e6c0924e2a061d10f72c892083eb45
Samson, Sumit <Sumit.Samson@ext.automic.com>
2016-04-28 16:08:31 +0530

    PMARA-2136<PMARA-2320>:commiting before merge with bugfix

--- tools/osbtool/resources/pythonscripts/pck_osb_execute_customization.py
+++ tools/osbtool/resources/pythonscripts/pck_osb_execute_customization.py
@@ -7,52 +7,74 @@ from java.io import FileInputStream
 
 import sys
 
+connFlag = False
+exitFlag = 1
+
 try:
+    try:
+        url = sys.argv[1]
+        username = sys.argv[2]
+        password = sys.argv[3]
+        connectionTimeout = sys.argv[4]
+        sessionName = sys.argv[5]
+        customFile = sys.argv[6]
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    url = sys.argv[3]
-    sessionName = sys.argv[4]
-    customFile = sys.argv[5]
+        # logging the inputs received
 
-    connect(username, password, url)
-    domainRuntime()
+        print 'URL : [%s]' % url
+        print 'Username : [%s]' % username
+        print 'Connection Timeout : [%s]' % connectionTimeout
+        print 'Session Name : [%s]' % sessionName
+        print 'Customization File : [%s]' % customFile
 
-    # obtain session management mbean to create a session.
-    # This mbean instance can be used more than once to
-    # create/discard/commit many sessions
+        # connect to OSB with URL endpoint, username, password, timeout
 
-    sessionMBean = findService(SessionManagementMBean.NAME,
-                               SessionManagementMBean.TYPE)
+        connect(username, password, url, timeout=connectionTimeout)
+        connFlag = True
 
-    # obtain the ALSBConfigurationMBean instance that operates
-    # on the session that has just been created. Notice that
-    # the name of the mbean contains the session name.
+        # to access the runtime MBean
 
-    alsbConfigurationMBean = findService(ALSBConfigurationMBean.NAME + '.'
-                              + sessionName,
-                              ALSBConfigurationMBean.TYPE)
+        domainRuntime()
 
-    # read a resource config file (example a jar) into bytes and uploading it
+        # obtain session management mbean to create a session.
 
-    if alsbConfigurationMBean is None:
-        print 'Session %s not found ' % sessionName
-        raise
-    
-    # customize if a customization file is specified
-    # affects only the created resources
+        sessionMBean = findService(SessionManagementMBean.NAME,
+                                   SessionManagementMBean.TYPE)
 
-    if customFile != None:
-        print 'Loading customization File', customFile
-        iStream = FileInputStream(customFile)
-        customizationList = Customization.fromXML(iStream)
-        alsbConfigurationMBean.customize(customizationList)
-except:
+        # obtain the ALSBConfigurationMBean instance that operates
+        # on the session that has just been created. Notice that
+        # the name of the mbean contains the session name.
 
-    print 'Unexpected error while importing resource : ', \
-        sys.exc_info()[0]
-    dumpStack()
-    raise
+        alsbConfigurationMBean = \
+            findService(ALSBConfigurationMBean.NAME + '.'
+                        + sessionName, ALSBConfigurationMBean.TYPE)
+
+        # read a resource config file (example a jar) into bytes and uploading it
+
+        if alsbConfigurationMBean is None:
+            print 'Session %s not found ' % sessionName
+            raise
+
+        # customizing
+
+        print 'Executing customization '
+
+        if customFile != None:
+            print 'Loading customization File', customFile
+            iStream = FileInputStream(customFile)
+            customizationList = Customization.fromXML(iStream)
+            alsbConfigurationMBean.customize(customizationList)
+            print 'Execution of customization done successfully'
+        exitFlag = 0
+    except:
+        print 'ERROR : Unable to create the session. Possible error:', \
+            sys.exc_info()[1]
+        print 'Please check the input parameters'
+finally:
+
+    if connFlag:
+        disconnect('true')
+    exit('y', exitFlag)
 
 
 			
