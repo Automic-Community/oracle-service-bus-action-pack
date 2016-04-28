3c74e62cc2b796ef25dd530afe6b0f27ce892ffc
unknown <vivekmalhotra@vivek2702.Nagarro.local>
2016-04-28 13:58:21 +0530

    PMARA-2153<PMARA-2326>: python exception handling

--- tools/osbtool/resources/pythonscripts/pck_osb_delete_project.py
+++ tools/osbtool/resources/pythonscripts/pck_osb_delete_project.py
@@ -6,18 +6,20 @@ from java.util import Collections
 from com.bea.wli.sb.util import Refs
 
 def deleteProject(alsbConfigurationMBean, projectName):
-     try:
-          projectRef = Ref(Ref.PROJECT_REF, Ref.DOMAIN, projectName)                  
-          if alsbConfigurationMBean.exists(projectRef):
-               print 'INFO: Removing OSB project: ' + projectName
-               alsbConfigurationMBean.delete(Collections.singleton(projectRef))
-               print 'INFO: Removed OSB project: ' + projectName
-          else:
-               raise AttributeError('No OSB project exists with name ' + projectName)
-     except:
-          raise
+	try:
+		projectRef = Ref(Ref.PROJECT_REF, Ref.DOMAIN, projectName)                  
+		if alsbConfigurationMBean.exists(projectRef):
+			print 'INFO: Removing OSB project: ' + projectName
+			alsbConfigurationMBean.delete(Collections.singleton(projectRef))
+			print 'INFO: Removed OSB project: ' + projectName
+		else:
+			raise ValueError('No OSB project exists with name ' + projectName)
+	except:
+		print 'ERROR: Exception occurred while deleting OSB project.', sys.exc_info()[1]
+		raise
 
 connectObj = False
+failStatus = 1
 try:
 	try:
 		username = sys.argv[1]
@@ -35,12 +37,13 @@ try:
 		alsbConfigurationMBean = findService(ALSBConfigurationMBean.NAME + "." + sessionName, ALSBConfigurationMBean.TYPE)
 		if alsbConfigurationMBean is not None:
 			deleteProject(alsbConfigurationMBean, projectName)
+			failStatus = 0
 		else:
-			raise AttributeError('No session exists with name ' + sessionName) 
+			raise ValueError('No session exists with name ' + sessionName)	
 	except:
-		print "ERROR : Unable to delete Project. Please check input parameters ", sys.exc_info()[0]
-		dumpStack()
-		raise
+		print 'ERROR: Unable to delete Project. Please check input parameters.', sys.exc_info()[1]
 finally:
-		if connectObj is True:
-			disconnect()	
+	if connectObj:
+		print 'INFO: Disconnecting from OSB ' + sys.argv[3]
+		disconnect()
+	exit(exitcode=failStatus)
