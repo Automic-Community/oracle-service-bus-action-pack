241deb8e5d67eee526df5d5c4f96ef76846f6f50
unknown <ankushpurwar@Ankush3578.Nagarro.local>
2016-04-29 20:20:48 +0530

    PCK_ORACLE_OSB-28:make actions to 12c compatible/documnetation update/import resource label update/fix discard action issue using non existing include

--- tools/osbtool/resources/pythonscripts/pck_osb_delete_project.py
+++ tools/osbtool/resources/pythonscripts/pck_osb_delete_project.py
@@ -1,9 +1,7 @@
 import wlstModule
-from com.bea.wli.sb.management.configuration import SessionManagementMBean
 from com.bea.wli.sb.management.configuration import ALSBConfigurationMBean
 from com.bea.wli.config import Ref
 from java.util import Collections
-from com.bea.wli.sb.util import Refs
 
 def deleteProject(alsbConfigurationMBean, projectName):
 	try:
